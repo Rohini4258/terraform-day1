dea9aec7db9bc9e4ff79937db855a025e0cd5e72
Rohini4258 <rohini4258@gmail.com>
2024-08-07 16:31:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
+provider "aws" {
+  region = "us-west-2"  
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0aff18ec83b712f05" 
   instance_type = "t2.micro"
   key_name = "key1"
-  region = "us-west-2"
 }
